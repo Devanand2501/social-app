875f46271caef85682ad0d5c80ab26730f2f7b7f
Devanand <devanandkangane@gmail.com>
2024-02-26 20:07:15 +0530

    updated model

--- social/musker/admin.py
+++ social/musker/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+
+# uregister group model
+admin.site.unregister(Group)
+
+# mix admin with profile 
+class ProfileToMix(admin.StackedInline):
+    model = Profile
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+    inlines = [ProfileToMix]
+
+# reregister user with only field username
+admin.site.unregister(User)
+admin.site.register(User,CustomUserAdmin)
+
+# register profile in admin site
+# admin.site.register(Profile)
