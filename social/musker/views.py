3a45ffe1299d0e625bdc408d968d7f1fb1d0cb49
Devanand <devanandkangane@gmail.com>
2024-02-27 18:58:38 +0530

    created profile list and updated the user model

--- social/musker/views.py
+++ social/musker/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Profile
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request,'index.html',{})
+
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html',{"profiles":profiles})
+    else:
+        messages.error(request,("Error: You need to login first"))
+        return redirect('home')
