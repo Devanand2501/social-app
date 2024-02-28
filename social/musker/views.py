f2cd438653871cd35eb5b1673eb051f74addf699
Devanand <devanandkangane@gmail.com>
2024-02-28 17:25:12 +0530

    Added follow and unfollow button

--- social/musker/views.py
+++ social/musker/views.py
@@ -18,6 +18,18 @@ def profile_list(request):
 def profile(request,pk):
     if request.user.is_authenticated:
         my_profile = Profile.objects.get(user_id = pk)
+
+        if request.method == "POST":
+            current_profile_user = request.user.profile
+            
+            action = request.POST.get('follow')
+            print("hello")
+
+            if action == "unfollow":
+                current_profile_user.follows.remove(my_profile)
+            elif action == "follow":
+                current_profile_user.follows.add(my_profile)
+            current_profile_user.save()
         return render(request,'profile.html',{ "my_profile" : my_profile })
     else:
         messages.error(request,("Erro: You are not loged in"))
